37eaf5a4e433304310aa7e69e689616aabd3ce70
xiz860817 <s0984008299@gmail.com>
2020-01-30 20:31:03 +0800

    TEST

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -9,9 +9,12 @@
           <table class="table table-hover m-0">
             <thead class="thead-darty">
               <tr>
-                <th>客戶編號</th>
-                <th>客戶姓名</th>
-                <th>客戶電話</th>
+                <th>員工編號</th>
+                <th>員工姓名</th>
+                <th>員工電話</th>
+                <th>員工地址</th>
+                <th>員工時薪</th>
+                <th>員工時數</th>
               </tr>
             </thead>
             <tbody>
@@ -20,8 +23,11 @@
                 foreach ($lists as $user){
                     echo "<tr>";
                     echo "<td>".$user->id."</td>";
-                    echo "<td>".$user->name."</td>";
+                    echo "<td>".$user->Name."</td>";
                     echo "<td>".$user->Phone."</td>";
+                    echo "<td>".$user->Address."</td>";
+                    echo "<td>".$user->HourlyPay."</td>";
+                    echo "<td>".$user->Hours."</td>";
                     echo "</tr>";
                 }
                 ?>                  
