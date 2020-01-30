ef407a92e37ff3fcfb2d028877f764045079f39c
xiz860817 <s0984008299@gmail.com>
2020-01-30 20:47:44 +0800

    新增主頁

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">50嵐華夏高鐵店</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="192.168.0.143/member">員工資料</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">查詢薪資</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">調整薪資</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">新增員工資料</a>
+      </li> 
+    </ul>
+  </div>  
+</nav>
+<br>
+
+<div class="Introduction">
+  <h3>好康報報</h3>
+  <div style="text-align:center;"><h4>來電、自取買五送一</h4></div>
+  <div style="text-align:center;"><h4>消費滿100，樂情外送</h4></div>
+  <h3>創店由來</h3>
+  <p>50嵐綠茶連鎖體系係由樸實性格的南部子弟馬瑞東先生，於西元1994年成立，經過六年認真奮鬥經營後，沒想到當初的無心插柳卻繁衍出一片亮麗的天空。
+馬先生從南自高雄區、中區(含雲、嘉)、進而向台北區、桃竹苗區陸續拓點，截至2008年3月底全省總店數超越三百多間門市，目前仍在陸續佈局中。
+而擁有著獨立、追求完美個性的中區加盟總部負責人-馬雅芬小姐，從其長兄馬瑞東先生手中接下中區經營權後，便於2002年3月在台中市健行路，開立了屬於自己的第一間50嵐健行總店。短短的五年內，更秉持著堅定的意志力及認真打拼的態度，在中區持續拓展近百間加盟店，也因此改寫了中區泡沫紅茶的歷史。</p>
+</div>
+
+</body>
+</html>
