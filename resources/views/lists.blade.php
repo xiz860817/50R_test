f81ebc0ff6203867cb7142e67099498f7d2d3508
xiz860817 <s0984008299@gmail.com>
2020-01-31 21:47:30 +0800

    test

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -1,6 +1,28 @@
 @extends('layouts.master')
 @section('title','員工資料')
 @section('content')
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">50嵐華夏高鐵店</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="member">員工資料</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">查詢日期</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">調整薪資</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">新增員工資料</a>
+      </li> 
+    </ul>
+  </div>  
+</nav>
   <div class="row justify-content-center">
     <div class="col-md-10">
       <div class="card">
