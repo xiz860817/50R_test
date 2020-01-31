6ec84ff06df8e6037dd438a877db88b8dfa7a08f
xiz860817 <s0984008299@gmail.com>
2020-01-31 22:00:14 +0800

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,28 +11,8 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <a class="navbar-brand" href="/">50嵐華夏高鐵店</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="member">員工資料</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">查詢日期</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">調整薪資</a>
-      </li>
-       <li class="nav-item">
-        <a class="nav-link" href="#">新增員工資料</a>
-      </li> 
-    </ul>
-  </div>  
-</nav>
+@include('partials.nav')
+
 <br>
 <div class="row justify-content-center">
     <div class="col-md-8">
