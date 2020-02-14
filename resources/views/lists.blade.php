2df02266298eee40a814c909ae61b526b41ce8cc
xiz860817 <s0984008299@gmail.com>
2020-02-14 16:05:36 +0800

    修改 lists

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -12,23 +12,22 @@
               <tr>
                 <th>員工編號</th>
                 <th>員工姓名</th>
-                <th>員工電話</th>
                 <th>員工地址</th>
+                <th>員工電話</th>
                 <th>員工時薪</th>
-                <th>員工時數</th>
               </tr>
             </thead>
             <tbody>
               
               <?php
-                foreach ($lists as $user){
+                foreach ($employees as $user){
                     echo "<tr>";
-                    echo "<td>".$user->id."</td>";
+                    echo "<td>".$user->Empid."</td>";
                     echo "<td>".$user->Name."</td>";
-                    echo "<td>".$user->Phone."</td>";
                     echo "<td>".$user->Address."</td>";
+                    echo "<td>".$user->Phone."</td>";
                     echo "<td>".$user->HourlyPay."</td>";
-                    echo "<td>".$user->Hours."</td>";
+                    
                     echo "</tr>";
                 }
                 ?>                  
