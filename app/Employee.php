578133826810d15fd9507994afde3059e428ba12
xiz860817 <s0984008299@gmail.com>
2020-02-15 08:58:41 +0800

    先不要設定帳號密碼

--- app/Employee.php
+++ app/Employee.php
@@ -9,7 +9,8 @@ class Employee extends Model
     //
     protected $table = 'employee';
     public $timestamps = false;
+    /*先不要有帳號密碼
     public function user(){
         return $this->belongsTo(UserEloquent::class);
-    }
+    }*/
 }
