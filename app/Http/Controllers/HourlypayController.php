36349a38fdb1fb28649fc09bfb82353c477deef3
xiz860817 <s0984008299@gmail.com>
2020-02-17 22:34:23 +0800

    test

--- app/Http/Controllers/HourlypayController.php
+++ app/Http/Controllers/HourlypayController.php
@@ -11,24 +11,12 @@ class HourlypayController extends Controller
     public function index(){
         //
         $h = Hourlypay::all();
-<<<<<<< HEAD
         return $h;
-        $s = new Hourlypay;
-        $s->Hourlypay = 160;
-        $s->save();
-        /*$emp = Hourlypay::find(1);
+
+        $emp = Hourlypay::find(1);
         echo $emp;
         //取得員工的時數
         echo $emp->employee->Name;
-        */
-=======
-        echo $h;
-
-        $hh = Hourlypay::find(2);
-        //echo $hh;
-        //取得員工的時數
-        //echo $hh->employee->Phone;
         
->>>>>>> 26ad0035df2aaa208d7cb6d15ada81aeb861c1ac
     }
 }
