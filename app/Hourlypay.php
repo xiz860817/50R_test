483e0b886b8740b91fbc9865bf1fbebc26b6645a
xiz860817 <s0984008299@gmail.com>
2020-02-17 22:36:48 +0800

    test

--- app/Hourlypay.php
+++ app/Hourlypay.php
@@ -13,10 +13,10 @@ class Hourlypay extends Model
     public $timestamps = false;
     
     public function employee(){
-<<<<<<< HEAD
+
         //return $this->belongsTo('App\Employee','id','id');
-=======
+
         return $this->belongsTo('App\Employee');
->>>>>>> 26ad0035df2aaa208d7cb6d15ada81aeb861c1ac
+
     }
 }
