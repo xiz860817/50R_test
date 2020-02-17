b9152dec19e236674e486c7ea4b5b01f19e89f00
xiz860817 <s0984008299@gmail.com>
2020-02-18 01:50:02 +0800

    test

--- app/Hours.php
+++ app/Hours.php
@@ -11,6 +11,6 @@ class Hours extends Model
     public $timestamps = false;
 
     public function date(){
-        return $this->belongsTo('App\Date','Date','date_id');
+        return $this->belongsTo('App\Date','date_id','Date');
     }
 }
