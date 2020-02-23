50d7895cbd4d92f9aecf4ccee268b41ac069516e
xiz860817 <s0984008299@gmail.com>
2020-02-23 20:08:28 +0800

    test

--- resources/views/showhours.blade.php
+++ resources/views/showhours.blade.php
@@ -7,14 +7,11 @@
         <div class="card">
         <div class="card-header">編輯客戶資料</div>
         <div class="card-body p-1">
-            <div class="form-group row">
-                <div class="col-sm-2"></div>
-                <label for="id" class="col-sm-2 col-form-label">員工編號</label>
                     <?php 
                     echo $_POST['month'];
                     ?>
-            </div>
+        </div>
         </div>
         </div>
     </div>
-</div>
+
