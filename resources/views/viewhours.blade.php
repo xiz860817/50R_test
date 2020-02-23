fce5f510c59af995f019f0856299f799cd4e8551
xiz860817 <s0984008299@gmail.com>
2020-02-23 16:37:57 +0800

    test

--- resources/views/viewhours.blade.php
+++ resources/views/viewhours.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
   <div class="row justify-content-center">
-    <div class="col-md-10">
+    <div class="col-md-4">
       <div class="card">
         <div class="card-header">查詢日期</div>
         <div class="card-body p-1">
