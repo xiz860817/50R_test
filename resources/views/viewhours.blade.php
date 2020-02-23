54ac41df544b0959f43a11b20717211e2b2ac03b
xiz860817 <s0984008299@gmail.com>
2020-02-23 20:27:44 +0800

    test

--- resources/views/viewhours.blade.php
+++ resources/views/viewhours.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 @section('title','查詢員工時數')
 @section('content')
+<form action="{{ action('HoursController@show') }}" method="post">
   <div class="row justify-content-center">
     <div class="col-md-4">
       <div class="card">
@@ -38,7 +39,16 @@
                     <th><h2>日</h2></th>
                 </tr>
           </table>
-          <td><a href="{{ action('HoursController@show') }}" method="post" class="btn btn-success btn-sm">查詢</a></td>
+          <div class="form-group row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <input type="submit" class="btn btn-primary" value="送出">
+                <input type="hidden" id="month" name="month"  value="<?php echo $_POST['month']; ?>">
+            </div>
+            <div class="col-sm-1">
+                <input type="submit" class="btn btn-warning" value="取消" name="cancel">
+            </div>
+        </div> 
         </div>
       </div>
     </div>
