a45d89f1e6f9e64ff0417109bc8ca4adc70b8cf5
xiz860817 <s0984008299@gmail.com>
2020-02-26 13:48:05 +0800

    test

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -43,7 +43,7 @@
                               'Name'=>$user->Name,
                               'Address'=>$user->Address,
                               'Phone'=>$user->Phone,
-                              'Hourlypay'=>$user->Hourlypay ])
+                              'Hourlypay'=>$user->Hourlypay ]),
                               action('DateController@delete', 
                               ['id'=>$date->id])}}" class="btn btn-danger btn-sm">刪除</a></td>       
                     <?php }  ?>             
