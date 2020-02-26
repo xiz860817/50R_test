9ce411cdb8c2d3b2aa62dc6b673e1dd267fe9b14
xiz860817 <s0984008299@gmail.com>
2020-02-26 13:12:47 +0800

    test

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -38,7 +38,7 @@
                     $dates = App\Date::where('employee_id','=',$user->id)->get();
                     foreach($dates as $date){ 
                       echo $date;?>
-                    <a href="{{ action('DateController@delete', 
+                    <a href="{{ action('DateController@index', 
                                 ['id'=>$date->id
                                  ])}}"
                                 
