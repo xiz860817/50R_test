2f46f78d263686600ceabaf12c7e85410f0c31c1
xiz860817 <s0984008299@gmail.com>
2020-02-27 01:53:08 +0800

    test

--- app/Http/Controllers/HoursController.php
+++ app/Http/Controllers/HoursController.php
@@ -12,13 +12,13 @@ class HoursController extends Controller
     //
     public function index(){
         $a = new Hours();
-        $a->date_id=7;
+        $a->date_id="7";
         $a->Hours = 8;
         $a->save();
-        $a->date_id=10;
+        $a->date_id="10";
         $a->Hours = 7;
         $a->save();
-        $a->date_id=11;
+        $a->date_id="11";
         $a->Hours = 6;
         $a->save();
         return View::make('viewhours');
