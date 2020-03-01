d56971477ae9c946e8fafc436909a1df188306ce
xiz860817 <s0984008299@gmail.com>
2020-03-01 20:37:33 +0800

    test

--- resources/views/clockon.blade.php
+++ resources/views/clockon.blade.php
@@ -1,3 +1,16 @@
-<?php
-echo 'hello';
-?>
+<!DOCTYPE html>
+<html>
+<body>
+	<button onclick="ThePrompt()">Prompt Box</button>
+	<span id="hellouser" style="color:blue;"></span>
+	<script>
+		function ThePrompt() {
+			var theuser = prompt("Please enter your name", "Nihsiang");
+			if (theuser != null) {
+				document.getElementById("hellouser").innerHTML =
+				"Hello " + theuser + "! ：)";
+			}
+		}
+	</script>
+</body>
+</html>
