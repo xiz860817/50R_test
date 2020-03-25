7779e6f4e1c1bd61a787a9f37ce88bf9a455630a
xiz860817 <s0984008299@gmail.com>
2020-03-25 18:48:04 +0800

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,5 +38,8 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
 }
