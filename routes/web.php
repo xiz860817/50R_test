2271642e821498bd4a967fa847ff313f84bee338
xiz860817 <s0984008299@gmail.com>
2020-03-25 22:04:37 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -15,43 +15,36 @@
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('login','Auth\LoginController@geyLogin')->middleware('guest');
-
-Route::middleware('auth')->group(function(){
-
-    Route::get('employee','EmployeeController@index');
-});
-
 Auth::routes();
 
-//Route::get('register','Auth\RegisterController@showRegistrationForm')->name('register');
-//Route::post('register','Auth\RegisterController@register');
-//員工新增刪除顯示資料
+//登入才能看的畫面
+Route::get('login','Auth\LoginController@geyLogin')->middleware('guest');
+
+//需要登入才能進入的頁面
+Route::middleware('auth')->group(function(){
+
+    //員工新增刪除顯示資料
+    Route::get('employee','EmployeeController@index');
+    Route::get('new', 'EmployeeController@new');
+    Route::post('store','EmployeeController@store');
+    Route::get('edit','EmployeeController@edit');
+    Route::post('update','EmployeeController@update');
+    Route::get('delete','EmployeeController@delete');
+
+    //查詢員工時數
+    Route::get('showhours','HoursController@show');
+    Route::get('date','HoursController@index');
+
+    //刪除員工連同->相同員工編碼的日期資料庫也刪除
+    Route::get('Datedelete','DateController@delete');
 
 
-//Route::get('employee','EmployeeController@index');
-Route::get('new', 'EmployeeController@new');
-Route::post('store','EmployeeController@store');
-Route::get('edit','EmployeeController@edit');
-Route::post('update','EmployeeController@update');
-Route::get('delete','EmployeeController@delete');
-
-//查詢員工時數
-Route::get('showhours','HoursController@show');
-Route::get('date','HoursController@index');
-
-//刪除員工連同->相同員工編碼的日期資料庫也刪除
-Route::get('Datedelete','DateController@delete');
-
-//打卡
-Route::get('clockon', function () {
-    return view('clockon');
 });
+
 //測試
 Route::get('test/create','DateController@create');
 Route::get('test','DateController@store');
 
-Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
