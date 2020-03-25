d8144c598d08b3721f2dabbbef68e68a58296bbf
xiz860817 <s0984008299@gmail.com>
2020-03-25 22:13:07 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 //登入才能看的畫面
-Route::get('login','Auth\LoginController@showLoginForm')->middleware('guest');
+Route::get('login','Auth\LoginController@showLoginForm')->name('login')->middleware('guest');
 
 //需要登入才能進入的頁面
 Route::middleware('auth')->group(function(){
